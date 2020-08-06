fe2958a8aaeb4cf7842e3a20aea58b8e7af8e197
jsw7524 <jsw7524@gmail.com>
2020-08-06 15:37:16 +0800

    demo57

--- demo57.py
+++ demo57.py
@@ -2,7 +2,7 @@ import numpy
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, GridSearchCV
 
 dataset1 = numpy.loadtxt('data/demo53_datasets.csv', delimiter=',', skiprows=1)
 print(type(dataset1), dataset1.shape)
@@ -13,20 +13,32 @@ print(inputList.shape)
 print(resultList.shape)
 
 
-def createModel():
+def createModel(layer1Count=8, layer2Count=10,
+                optimizer='adam', init='uniform'):
     # global model
     model = Sequential()
     print(type(model))
-    model.add(Dense(8, input_dim=8, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(layer1Count, input_dim=8, kernel_initializer=init, activation='relu'))
+    model.add(Dense(layer2Count, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     print(model.summary())
     return model
 
 
-model1 = KerasClassifier(build_fn=createModel, epochs=200, batch_size=20, verbose=0)
-fiveFold = StratifiedKFold(n_splits=5, shuffle=True)
-results = cross_val_score(model1, inputList, resultList, cv=fiveFold)
-print(results)
-print(f"mean={results.mean()},std={results.std()}")
+model1 = KerasClassifier(build_fn=createModel, verbose=0)
+optimizers = ['rmsprop', 'adam', 'sgd']
+inits = ['normal', 'uniform']
+epochs = [50, 100, 150]
+batches = [5, 10, 15]
+layer1Counts = [8, 10, 12, 14]
+layer2Counts = [10, 20, 30, 40]
+param_grid = dict(optimizer=optimizers, epochs=epochs, batch_size=batches, init=inits,
+                  layer1Count=layer1Counts, layer2Count=layer2Counts)
+grid = GridSearchCV(estimator=model1, param_grid=param_grid)
+grid_result = grid.fit(inputList, resultList)
+
+print("Best %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+
+grid = GridSearchCV(estimator=model1, param_grid=param_grid, cv=3)
